2fe940dc154be11ea457492f4c43b22fe6eaa985
godjango <mironkez@yandex.ru>
2021-02-11 00:36:02 +0500

    return SECRET_KEY. It is not production

--- django_test_motion/settings.py
+++ django_test_motion/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '_w+3h+%d(l8(_vu!l%wis&8o&0%4%(sr1v)trxsvvci3&w7_+l'
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = '_w+3h+%d(l8(_vu!l%wis&8o&0%4%(sr1v)trxsvvci3&w7_+l'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
